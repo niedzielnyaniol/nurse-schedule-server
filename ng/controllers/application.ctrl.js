4a3f8575770852b2163cc37d0f6180410edcdd36
Maciek Kaczmarczyk <maciejkaczmarczyk95@gmail.com>
2017-04-20 22:16:29 +0200

    update: simple front layout

--- ng/controllers/application.ctrl.js
+++ ng/controllers/application.ctrl.js
@@ -1,3 +1,19 @@
 angular.module('app')
-    .controller('ApplicationController', function($scope){
-    });
+    .controller('ApplicationController', function($scope, ApplicationService){
+    
+    $scope.isDisabled = false;
+    $scope.isHidden = false;
+    
+    $scope.generateSchedule = function(){
+        
+        $scope.isDisabled = true;
+        
+        ApplicationService.fetchData()
+            .then(function success(data){
+                console.log(data.data);
+                $scope.isHidden = true;
+                $scope.calendar = data.data;
+        });
+    };
+         
+});
--- ng/services/application.svc.js
+++ ng/services/application.svc.js
@@ -1,4 +1,7 @@
 angular.module('app')
     .service('ApplicationService', function($http){
-        
-    });
+
+    this.fetchData = function(){
+        return $http.get('/api/faker');
+    }         
+});
