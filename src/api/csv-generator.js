635d7c5465dbb97b96f24f5db0addabeadf3060f
Maciek Kaczmarczyk <maciej.kaczmarczyk@fingoweb.com>
2017-06-04 22:51:58 +0200

    update: parsing file to csv

--- src/api/csv-generator.js
+++ src/api/csv-generator.js
@@ -1,28 +1,25 @@
+/* eslint angular/json-functions:0 */
+
 const router = require('express').Router();
 const path = require('path');
+const request = require('request');
+const _ = require('lodash');
 
 const json2csv = require('json2csv');
 const fs = require('fs');
 
 router.get('/', (req, res) => {
-  const fields = ['car', 'price', 'color'];
-  const myCars = [
-    {
-      car: 'Audi',
-      price: 40000,
-      color: 'blue'
-    }, {
-      car: 'BMW',
-      price: 35000,
-      color: 'black'
-    }, {
-      car: 'Porsche',
-      price: 60000,
-      color: 'green'
+  request('http://localhost:3000/api/faker', (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      createFile(JSON.parse(body), res);
     }
-  ];
+  });
+});
 
-  const csv = json2csv({ data: myCars, fields, del: ';' });
+const createFile = (data, res) => {
+  const fields = ['number', 'day', 'shifts'];
+  const fieldNames = ['#', 'Day', 'Shifts'];
+  const csv = json2csv({ data, fields, fieldNames, del: ';' });
 
   fs.writeFile('nurse-sheduler.csv', csv, (err) => {
     if (err) {
@@ -31,7 +28,6 @@ router.get('/', (req, res) => {
 
     res.download(path.join(__dirname, '../../nurse-sheduler.csv'));
   });
-
-});
+};
 
 module.exports = router;
